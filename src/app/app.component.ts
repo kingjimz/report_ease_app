0cce0897cc766e3fcd83f8c3fc5f4d1d2b81c71c
King Jims <dacanaykingjims@gmail.com>
2025-06-16 12:57:56 +0800

    angular pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { Auth, onAuthStateChanged } from '@angular/fire/auth';
 import { Router, RouterOutlet } from '@angular/router';
 import { ApiService } from './_services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterOutlet]
+  imports: [RouterOutlet, CommonModule]
 })
 export class AppComponent implements OnInit {
-  constructor(private auth: Auth, private router: Router, private api: ApiService) {}
+  deferredPrompt: any;
+  showInstallButton = false;
+
+  constructor(private auth: Auth, private router: Router, private api: ApiService) {
+
+  }
 
   ngOnInit() {
+     window.addEventListener('beforeinstallprompt', (event) => {
+      event.preventDefault();
+      this.deferredPrompt = event;
+      this.showInstallButton = true;
+    });
+    
     onAuthStateChanged(this.auth, (user) => {
       if (user) {
         this.router.navigateByUrl('/');
@@ -29,5 +41,18 @@ export class AppComponent implements OnInit {
     }).catch(error => {
       console.error('Error fetching Bible studies:', error);
     });
+  }
+
+  installPWA() {
+    if (this.deferredPrompt) {
+      this.deferredPrompt.prompt();
+      this.deferredPrompt.userChoice.then((choiceResult: any) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('PWA setup accepted');
+        }
+        this.deferredPrompt = null;
+        this.showInstallButton = false;
+      });
+    }
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { environment } from '../environments/environment';
 import { routes } from './app.routes';
@@ -8,12 +8,16 @@ import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 //Firebase
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideServiceWorker } from '@angular/service-worker';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
-    provideAuth(() => getAuth())
+    provideAuth(() => getAuth()), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 };
